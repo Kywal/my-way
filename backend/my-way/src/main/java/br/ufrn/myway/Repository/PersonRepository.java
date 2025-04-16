ea0108ee494ff3968ac18b698d4cb643a638b49b
Ewerton <eww999@hotmail.com>
2025-04-16 20:31:02 -0300

    feat: implementing softdeletablerepository and abstractmodel

--- backend/my-way/src/main/java/br/ufrn/myway/Repository/PersonRepository.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Repository/PersonRepository.java
@@ -1,12 +1,13 @@
 package br.ufrn.myway.Repository;
 
 import br.ufrn.myway.Model.Entities.Person;
+import br.ufrn.myway.Model.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {
+public interface PersonRepository extends SoftDeletableRepository<Person> {
 
 }
---
+++ backend/my-way/src/main/java/br/ufrn/myway/Repository/SoftDeletableRepository.java
@@ -0,0 +1,26 @@
+package br.ufrn.myway.Repository;
+
+import br.ufrn.myway.Model.Entities.AbstractModel;
+import br.ufrn.myway.Model.Entities.Person;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+@NoRepositoryBean
+public interface SoftDeletableRepository<M extends AbstractModel> extends AbstractRepository<M> {
+    @Modifying
+    @Transactional
+    @Query("UPDATE #{#entityName} e SET e.ativo = false WHERE e.id = :id")
+    public void delete(@Param("id")Long id);
+
+    @Query("SELECT e FROM #{#entityName} e WHERE e.ativo = true")
+    public List<M> list();
+
+    @Query("SELECT e FROM #{#entityName} e WHERE e.ativo = true AND e.id = :id")
+    @Override
+    public M getById(@Param("id")Long id);
+}
