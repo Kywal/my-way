e3cdb9d0ca082cff9ad3548515b18af484a90dd7
alisson.diogo.soares <alisson.diogo.soares@gmail.com>
2025-04-16 00:52:09 -0300

    feat(frontend): add new my way logo

--- frontend/my-way/components/navbar.tsx
+++ frontend/my-way/components/navbar.tsx
@@ -14,6 +14,9 @@ import { Input } from "@heroui/input";
 import { link as linkStyles } from "@heroui/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
+import Image from "next/image";
+
+import logoMyWay from "../public/my-way-logo.png";
 
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
@@ -23,7 +26,6 @@ import {
   DiscordIcon,
   HeartFilledIcon,
   SearchIcon,
-  Logo,
 } from "@/components/icons";
 
 export const Navbar = () => {
@@ -53,8 +55,12 @@ export const Navbar = () => {
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <Image
+              alt="logo do app my way"
+              src={logoMyWay}
+              className="rounded-lg"
+              width={40}
+            />
           </NextLink>
         </NavbarBrand>
         <ul className="hidden lg:flex gap-4 justify-start ml-2">
