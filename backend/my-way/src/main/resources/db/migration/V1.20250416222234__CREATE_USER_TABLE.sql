0baf72bc08e144eaf9ce3fa5bb3d62fda48c039c
alisson.diogo.soares <alisson.diogo.soares@gmail.com>
2025-04-16 22:31:35 -0300

    feat(backend): tb_user migration

---
+++ backend/my-way/src/main/resources/db/migration/V1.20250416222234__CREATE_USER_TABLE.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tb_user (
+    id BIGINT PRIMARY KEY,
+    id_person BIGINT,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    tokens INT NOT NULL,
+    CONSTRAINT fk_user_person FOREIGN KEY (id_person)
+        REFERENCES person (id)
+        ON DELETE CASCADE
+);
