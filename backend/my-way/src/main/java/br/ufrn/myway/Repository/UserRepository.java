541fc0c5338a1c38a14accf8d8a08846a5acae6b
Emanuel Kywal <emanuelkywal@outlook.com>
2025-04-17 19:42:50 -0300

    feat: findByEmail on userRepository

--- backend/my-way/src/main/java/br/ufrn/myway/Repository/UserRepository.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Repository/UserRepository.java
@@ -1,14 +1,13 @@
 package br.ufrn.myway.Repository;
 
 import br.ufrn.myway.Model.Entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends SoftDeletableRepository<User> {
+
+    Optional<User> findByEmail(String email);
 
 }
