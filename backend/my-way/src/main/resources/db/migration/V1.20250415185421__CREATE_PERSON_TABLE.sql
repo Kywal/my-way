e2a0e34146cc9d4bb166c92d3f4f17bc33a49a15
alisson.diogo.soares <alisson.diogo.soares@gmail.com>
2025-04-18 01:50:18 -0300

    fix(backend): delete unused migrations

--- backend/my-way/src/main/resources/db/migration/V1.20250415185421__CREATE_PERSON_TABLE.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE tb_person (
-    id BIGINT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    birthDate DATE NOT NULL,
-    gender VARCHAR(50),
-    history_description TEXT,
-    country VARCHAR(100),
-    region VARCHAR(100)
-);
