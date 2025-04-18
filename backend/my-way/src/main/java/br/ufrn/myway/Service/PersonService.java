ce3bef9cb5a1ff07ef663ab419059338ad314c5f
Ewerton <eww999@hotmail.com>
2025-04-18 14:09:21 -0300

    feat: add password encryption

--- backend/my-way/src/main/java/br/ufrn/myway/Service/PersonService.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Service/PersonService.java
@@ -3,6 +3,7 @@ package br.ufrn.myway.Service;
 import br.ufrn.myway.Model.Person;
 import br.ufrn.myway.Repository.PersonRepository; 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
  
 import java.util.List; 
