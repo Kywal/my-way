5ec73a0fb4929c834e0c49fd4297e88094fd23d7
alisson.diogo.soares <alisson.diogo.soares@gmail.com>
2025-04-19 13:28:24 -0300

    fix(backend): add other frontend port that can be used

--- backend/my-way/src/main/java/br/ufrn/myway/Config/SecurityConfig.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Replace with your frontend URL
+                        .allowedOrigins("http://localhost:3000", "http://localhost:3001") // Replace with your frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
