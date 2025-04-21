d72c6c24aa4b3348ad8e218215fef8d46914eda3
Ewerton <eww999@hotmail.com>
2025-04-21 15:18:03 -0300

    refact: chaing exception to runtimeexception on businessexception

--- backend/my-way/src/main/java/br/ufrn/myway/Model/Enums/ErrorMessageUtils.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Model/Enums/ErrorMessageUtils.java
@@ -5,7 +5,6 @@ import br.ufrn.myway.Service.BusinessException;
 import java.text.MessageFormat;
 
 public enum ErrorMessageUtils {
-
     ERROR_NOT_FOUND("{0} not found."),
     ERROR_ALREADY_EXISTS("{0} already exists with this {1}.");
 
