692e6a1fa16a710daf2c71d4825b84f3d70888ff
Emanuel Kywal <emanuelkywal@outlook.com>
2025-04-22 07:53:33 -0300

    fix(backend): remove unused imports on UserService

--- backend/my-way/src/main/java/br/ufrn/myway/Service/UserService.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Service/UserService.java
@@ -1,11 +1,9 @@
 package br.ufrn.myway.Service;
-import br.ufrn.myway.Model.Entities.User;
+
 import br.ufrn.myway.Model.Enums.ErrorMessageUtils;
 import br.ufrn.myway.Model.User;
 import br.ufrn.myway.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
