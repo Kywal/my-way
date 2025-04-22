66fa60b8cacd937a917d3978ca0ce3e60e4a64a7
Emanuel Kywal <emanuelkywal@outlook.com>
2025-04-22 08:36:21 -0300

    feat(backend): Roadmap and Goal attributes

--- backend/my-way/src/main/java/br/ufrn/myway/Model/Goal.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Model/Goal.java
@@ -1,9 +1,52 @@
 package br.ufrn.myway.Model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_goal")
 public class Goal extends AbstractModel {
+
+    private String name;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "id_roadmap")
+    private Roadmap roadmap;
+
+    public Goal() {
+    }
+
+    public Goal(String name, String description, Roadmap roadmap) {
+        this.name = name;
+        this.description = description;
+        this.roadmap = roadmap;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Roadmap getRoadmap() {
+        return roadmap;
+    }
+
+    public void setRoadmap(Roadmap roadmap) {
+        this.roadmap = roadmap;
+    }
 }
