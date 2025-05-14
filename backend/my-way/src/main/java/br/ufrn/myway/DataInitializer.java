df7b3aa1ae802671f43a9d17d66143bf843b3d3d
alisson.diogo.soares <alisson.diogo.soares@gmail.com>
2025-05-13 23:22:00 -0300

    feat(backend): na inicialização do sistema cria uma missão diária default, para ser usada nos testes

---
+++ backend/my-way/src/main/java/br/ufrn/myway/DataInitializer.java
@@ -0,0 +1,33 @@
+package br.ufrn.myway;
+
+import br.ufrn.myway.Model.Entities.Mission;
+import br.ufrn.myway.Model.Enums.MissionFrequency;
+import br.ufrn.myway.Model.Enums.MissionType;
+import br.ufrn.myway.Repository.MissionRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements ApplicationRunner {
+    private final MissionRepository missionRepository;
+
+    public DataInitializer(MissionRepository missionRepository) {
+        this.missionRepository = missionRepository;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        if (missionRepository.count() == 0) {
+            Mission defaultMission = new Mission();
+            defaultMission.setTitle("Estudar na plataforma");
+            defaultMission.setDescription("Estude no MyWay usando o sistema de estuos pomodoro.");
+            defaultMission.setFrequency(MissionFrequency.DAILY);
+            defaultMission.setType(MissionType.QUANTITY_TIME);
+            defaultMission.setTimeInMinutes(10);
+            defaultMission.setRewardPoints(15);
+
+            missionRepository.save(defaultMission);
+        }
+    }
+}
