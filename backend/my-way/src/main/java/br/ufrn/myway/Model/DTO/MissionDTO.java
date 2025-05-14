6a43efb30b929211a5ea63f6ca915d732f7a3722
alisson.diogo.soares <alisson.diogo.soares@gmail.com>
2025-05-13 23:21:29 -0300

    fix(backend): corrige missionDTO após remover atributos

--- backend/my-way/src/main/java/br/ufrn/myway/Model/DTO/MissionDTO.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Model/DTO/MissionDTO.java
@@ -1,20 +1,11 @@
 package br.ufrn.myway.Model.DTO;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-import br.ufrn.myway.Model.Entities.User;
-
 public record MissionDTO(
         String title,
         String description,
         int rewardPoints,
-        LocalDateTime startDate,
-        LocalDateTime endDate,
-        boolean completed,
         Integer quantityGoal,
-        Integer timeInMinutes,
-        List<User> users
+        Integer timeInMinutes
         ) {
 
 }
