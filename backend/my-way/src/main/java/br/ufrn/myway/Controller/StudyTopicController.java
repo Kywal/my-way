a7d71f7d4fe93116641addd3439bc2a866578615
carvalhoewerton <eww999@hotmail.com>
2025-05-20 15:46:17 -0300

    fix: fixing goal,roadmap and studytopic controllers

--- backend/my-way/src/main/java/br/ufrn/myway/Controller/StudyTopicController.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Controller/StudyTopicController.java
@@ -1,9 +1,10 @@
 package br.ufrn.myway.Controller;
 
+import br.ufrn.myway.Model.DTO.StudyTopicDTO;
 import br.ufrn.myway.Model.Entities.StudyTopic;
+import br.ufrn.myway.Model.Mapper.StudyTopicMapper;
 import br.ufrn.myway.Service.StudyTopicService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,16 +12,31 @@ import java.util.List;
 @RestController
 @RequestMapping("/studytopic")
 public class StudyTopicController {
+
     @Autowired
     private StudyTopicService studyTopicService;
 
-    @PostMapping("/create")
-    public ResponseEntity<StudyTopic> create(@RequestBody StudyTopic newStudyTopic){
-        StudyTopic studyTopic = studyTopicService.createStudyTopic(newStudyTopic);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    @Autowired
+    private StudyTopicMapper studyTopicMapper;
+
+    @PostMapping("/save")
+    public ResponseEntity<StudyTopicDTO> save(@RequestBody StudyTopicDTO studyTopicDto){
+        StudyTopic studyTopic = studyTopicMapper.toEntity(studyTopicDto);
+        return ResponseEntity.ok(studyTopicMapper.toDto(studyTopicService.save(studyTopic)));
     }
-    @GetMapping
-    public List<StudyTopic> listStudyTopics(){
-        return studyTopicService.listStudyTopic();
+    @GetMapping("/list")
+    public List<StudyTopicDTO> list(){
+        return studyTopicMapper.toListDTO(studyTopicService.list());
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<StudyTopicDTO> get(@PathVariable Long id){
+        return ResponseEntity.ok(studyTopicMapper.toDto(studyTopicService.findById(id)));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id){
+        studyTopicService.delete(id);
+        return ResponseEntity.ok("Study Topic successfully deleted.");
     }
 }
