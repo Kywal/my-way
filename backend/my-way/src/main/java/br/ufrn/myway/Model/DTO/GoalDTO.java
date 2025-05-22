067189f290aa108ea12770c680ed24e6c1cc9a97
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-22 14:54:44 -0300

    refact(backend): removes and refacts params at GoalDTO
    removes params id and createdAt
    refact param roadmap to roadmapId

--- backend/my-way/src/main/java/br/ufrn/myway/Model/DTO/GoalDTO.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Model/DTO/GoalDTO.java
@@ -3,5 +3,5 @@ package br.ufrn.myway.Model.DTO;
 import java.time.LocalDate;
 import java.util.List;
 
-public record GoalDTO(Long id, LocalDate createdAt, String name, RoadMapSimpleDTO roadMap, int size, List<StudyTopicDTO> exercises) {
+public record GoalDTO(String name, Long roadmapId, int size, List<StudyTopicDTO> exercises) {
 }
