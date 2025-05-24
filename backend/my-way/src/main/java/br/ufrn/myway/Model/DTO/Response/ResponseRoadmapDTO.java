5d1f424f0ba077138619fc9a25c1377c1779b8ff
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-24 18:23:01 -0300

    feat(backend): ResponseRoadmapDTO attributes id and createdAt
    change goals attribute type to ResponseGoalDTO

--- backend/my-way/src/main/java/br/ufrn/myway/Model/DTO/Response/ResponseRoadmapDTO.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Model/DTO/Response/ResponseRoadmapDTO.java
@@ -1,9 +1,9 @@
 package br.ufrn.myway.Model.DTO.Response;
 
-import br.ufrn.myway.Model.DTO.GoalDTO;
 import br.ufrn.myway.Model.Enums.RoadMapStatus;
 
+import java.time.LocalDate;
 import java.util.List;
 
-public record ResponseRoadmapDTO(String mainGoal, String description, List<GoalDTO> goals, RoadMapStatus status) {
+public record ResponseRoadmapDTO(Long id, LocalDate createdAt, String mainGoal, String description, List<ResponseGoalDTO> goals, RoadMapStatus status) {
 }
