91f3722250f6db298e03b1e65dd8b997bf2d18a3
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-25 20:59:51 -0300

    feat(backend): RoadmapAiService class and createRoadmapWithAi method

---
+++ backend/my-way/src/main/java/br/ufrn/myway/Service/RoadmapService/RoadmapAiService.java
@@ -0,0 +1,32 @@
+package br.ufrn.myway.Service.RoadmapService;
+
+import br.ufrn.myway.Model.Entities.Roadmap;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoadmapAiService {
+
+
+    @Autowired
+    private RoadmapService roadmapService;
+
+    @Autowired
+    private ChatClient chatClient;
+
+    @Autowired
+    private ChatModel chatModel;
+
+    public Roadmap createRoadmapWithAi(String mainGoal, String description) {
+        String prompt = "Gere um roadmap para o objetivo: {mainGoal}, a descrição desse objetivo é: {description}.";
+
+        return ChatClient.create(chatModel).prompt()
+                .user(u -> u.text(prompt)
+                        .param(mainGoal, description))
+                .call()
+                .entity(Roadmap.class);
+    }
+
+}
