4777756f57a3ea26aab2b2f105c27b4c08996107
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-25 23:17:55 -0300

    fix(backend): error during createdAt serialization at AI generateRoadmap method

--- backend/my-way/src/main/java/br/ufrn/myway/Model/Entities/AbstractModel.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Model/Entities/AbstractModel.java
@@ -20,7 +20,7 @@ public abstract class AbstractModel {
     protected Long id;
 
     @Column(updatable = false)
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd['T'HH:mm:ss]")
     protected LocalDate createdAt = LocalDate.now();
 
     protected boolean ativo = true;
