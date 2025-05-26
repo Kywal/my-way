8a8498236ab1166a205d0ce18fc612e3eb44d818
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-25 23:52:03 -0300

    feat(backend): AiRoadmapController class and generateRoadmap endpoint

---
+++ backend/my-way/src/main/java/br/ufrn/myway/Controller/RoadmapController/AiRoadmapController.java
@@ -0,0 +1,34 @@
+package br.ufrn.myway.Controller.RoadmapController;
+
+import br.ufrn.myway.Model.DTO.GenerateRoadmapDTO;
+import br.ufrn.myway.Model.DTO.Request.RequestRoadmapDTO;
+import br.ufrn.myway.Model.Mapper.RoadmapMapper;
+import br.ufrn.myway.Service.RoadmapService.AiRoadmapService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ai/roadmap")
+public class AiRoadmapController {
+
+    @Autowired
+    private AiRoadmapService aiRoadmapService;
+
+    @Autowired
+    private RoadmapMapper roadmapMapper;
+
+    @PostMapping
+    public ResponseEntity<GenerateRoadmapDTO> generateRoadmap(@RequestBody RequestRoadmapDTO roadmapDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                aiRoadmapService.generateRoadmap(
+                    roadmapDTO.mainGoal(), roadmapDTO.description()
+                )
+        );
+    }
+
+}
