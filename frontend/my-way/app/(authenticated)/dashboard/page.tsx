3ebe8ef501d1a636a08dceb910d339dc83d08df7
alisson.diogo.soares <alisson.diogo.soares@gmail.com>
2025-05-27 10:28:15 -0300

    feat(frontend): show floatButton and modalRegisterRoadmap in dashboard

--- frontend/my-way/app/(authenticated)/dashboard/page.tsx
+++ frontend/my-way/app/(authenticated)/dashboard/page.tsx
@@ -4,9 +4,13 @@ import React from "react";
 import { useSession } from "next-auth/react";
 
 import { Roadmap } from "@/components/Roadmap";
+import { FloatButton } from "@/components/floatButton";
+import { useDisclosure } from "@heroui/react";
+import ModalRegisterRoadmap from "@/components/modalRegisterRoadmap";
 
 export default function DashboardPage() {
   const { data: session, status } = useSession();
+  const { isOpen, onOpenChange, onClose, onOpen } = useDisclosure();
 
   if (status === "loading") {
     return <p>Carregando sessão...</p>;
@@ -19,6 +23,8 @@ export default function DashboardPage() {
   return (
     <div>
       <Roadmap />
+      <FloatButton onClick={onOpen} />
+      <ModalRegisterRoadmap isOpen={isOpen} onOpenChange={onOpenChange} />
     </div>
   );
 }
