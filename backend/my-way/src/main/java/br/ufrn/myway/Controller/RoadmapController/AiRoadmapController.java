0ff1cc2b0a19fcfbec94d3159d2b8a4cd059cebf
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-27 14:47:09 -0300

    feat(backend): generateAndSaveRoadmap endpoint

--- backend/my-way/src/main/java/br/ufrn/myway/Controller/RoadmapController/AiRoadmapController.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Controller/RoadmapController/AiRoadmapController.java
@@ -1,12 +1,15 @@
 package br.ufrn.myway.Controller.RoadmapController;
 
+import br.ufrn.myway.Model.DTO.Request.RequestFullRoadmapDTO;
 import br.ufrn.myway.Model.DTO.Request.RequestRoadmapDTO;
 import br.ufrn.myway.Model.DTO.Response.ResponseGenerateRoadmapDTO;
+import br.ufrn.myway.Model.DTO.Response.ResponseRoadmapDTO;
 import br.ufrn.myway.Model.Mapper.RoadmapMapper;
 import br.ufrn.myway.Service.RoadmapService.AiRoadmapService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +30,19 @@ public class AiRoadmapController {
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 aiRoadmapService.generateRoadmap(
                     roadmapDTO.mainGoal(), roadmapDTO.description()
+                )
+        );
+    }
+
+    @PostMapping("/gen-and-save/{userId}")
+    public ResponseEntity<ResponseRoadmapDTO> generateAndSaveRoadmap(@RequestBody RequestFullRoadmapDTO roadmapDTO, @PathVariable Long userId) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                roadmapMapper.toResponse(
+                        aiRoadmapService.generateAndSaveRoadmap(
+                                roadmapDTO.mainGoal(),
+                                roadmapDTO.description(),
+                                userId
+                        )
                 )
         );
     }
