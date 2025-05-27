bb23a05b0b0b1bfb758544ef07234b5161d0f34d
Ewerton <eww999@hotmail.com>
2025-05-27 17:58:56 -0300

    fix: add get roadmap by status method and get active roadmap endpoint

--- backend/my-way/src/main/java/br/ufrn/myway/Repository/RoadmapRepository.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Repository/RoadmapRepository.java
@@ -1,11 +1,17 @@
 package br.ufrn.myway.Repository;
 
 import br.ufrn.myway.Model.Entities.Roadmap;
+import br.ufrn.myway.Model.Enums.RoadMapStatus;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface RoadmapRepository extends SoftDeletableRepository<Roadmap> {
     @Query("SELECT r FROM Roadmap r WHERE r.user.id = :id")
     List<Roadmap> findRoadMapByUser(Long id);
+
+    @Query("SELECT r FROM Roadmap r WHERE r.user.id = :id AND r.status = :status")
+    Roadmap findByStatus(@Param("id") Long id, @Param("status") RoadMapStatus status);
+
 }
