098d28e3014d02e19557e85980f6818969e21684
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-27 22:04:42 -0300

    fix(backend): null verification of user on save method at RoadmapService

--- backend/my-way/src/main/java/br/ufrn/myway/Service/RoadmapService/RoadmapService.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Service/RoadmapService/RoadmapService.java
@@ -33,7 +33,7 @@ public class RoadmapService {
 
     public Roadmap save(Roadmap roadmap, Long id) {
         User user = userService.findById(id);
-        if (user == null) {
+        if (user != null) {
             roadmap.setUser(user);
         }
         if (roadmap.getStatus() == null) {
