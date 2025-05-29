21da3a3715c977caa0a2b177a84fe99302366f0d
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-29 09:30:28 -0300

    refact(backend): removes roadmapIndex from endpoint

--- backend/my-way/src/main/java/br/ufrn/myway/Controller/GoalController/AiGoalController.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Controller/GoalController/AiGoalController.java
@@ -1,6 +1,6 @@
 package br.ufrn.myway.Controller.GoalController;
 
-import br.ufrn.myway.Model.DTO.Response.ResponseGenerateGoalDTO;
+import br.ufrn.myway.Model.DTO.Response.ResponseGoalDTO;
 import br.ufrn.myway.Model.Mapper.GoalMapper;
 import br.ufrn.myway.Service.GoalService.AiGoalService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,9 +22,11 @@ public class AiGoalController {
     private GoalMapper goalMapper;
 
     @PostMapping("/gen-and-save/{roadmapId}")
-    public ResponseEntity<ResponseGenerateGoalDTO> generateAndSave(@PathVariable Long roadmapId, @RequestBody Long roadmapIndex) {
+    public ResponseEntity<ResponseGoalDTO> generateAndSave(@PathVariable Long roadmapId) {
         return ResponseEntity.status(HttpStatus.CREATED).body(
-                aiGoalService.generateGoal(roadmapId, roadmapIndex)
+                goalMapper.toResponse(
+                        aiGoalService.generateAndSave(roadmapId)
+                )
         );
     }
 
