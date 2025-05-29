18ebd3a22e0d99fc9153f6d595289c7df5890491
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-29 09:29:43 -0300

    feat(backend): methods saveGenerated and generateAndSave at AiGoalService
    removes roadmapIndex and set prompt to generate a new last goal

--- backend/my-way/src/main/java/br/ufrn/myway/Service/GoalService/AiGoalService.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Service/GoalService/AiGoalService.java
@@ -1,10 +1,14 @@
 package br.ufrn.myway.Service.GoalService;
 
 import br.ufrn.myway.Model.DTO.Response.ResponseGenerateGoalDTO;
+import br.ufrn.myway.Model.Entities.Goal;
 import br.ufrn.myway.Model.Entities.Roadmap;
+import br.ufrn.myway.Model.Entities.StudyTopic;
 import br.ufrn.myway.Model.Enums.ErrorMessageUtils;
+import br.ufrn.myway.Model.Mapper.GoalMapper;
 import br.ufrn.myway.Service.BusinessException;
 import br.ufrn.myway.Service.RoadmapService.RoadmapService;
+import br.ufrn.myway.Service.StudyTopicService;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,30 +22,60 @@ public class AiGoalService {
     private RoadmapService roadmapService;
 
     @Autowired
-    private ChatModel chatModel;
+    private GoalService goalService;
 
-    public ResponseGenerateGoalDTO generateGoal(Long roadmapId, Long roadmapIndex){
+    @Autowired
+    private GoalMapper goalMapper;
+
+    @Autowired
+    private ChatModel chatModel;
+    @Autowired
+    private StudyTopicService studyTopicService;
+
+    public ResponseGenerateGoalDTO generateGoal(Roadmap roadmap){
+        String prompt =
+                """
+                Gere um novo último objetivo para o seguinte roadmap: \n{roadmap}
+                """;
+
+        return ChatClient.create(chatModel).prompt()
+                .user(u ->
+                        u.text(prompt).param("roadmap", roadmap.toString())
+                )
+                .call()
+                .entity(ResponseGenerateGoalDTO.class);
+    }
+
+    public Goal saveGenerated(Roadmap roadmap, Goal goal) {
+        Goal goalToBeSaved = new Goal();
+        goalToBeSaved.setDescription(goal.getDescription());
+        goalToBeSaved.setName(goal.getName());
+        goalToBeSaved.setRoadmap(roadmap);
+        goalToBeSaved = goalService.save(goalToBeSaved, roadmap.getId());
+
+        goal.setRoadmap(roadmap);
+        goal.setId(goalToBeSaved.getId());
+        goal.setRoadmapIndex(goalToBeSaved.getRoadmapIndex());
+
+        for (StudyTopic studyTopic : goal.getStudyTopics()) {
+            studyTopic.setGoal(goalToBeSaved);
+            studyTopicService.save(studyTopic, goalToBeSaved.getId());
+        }
+
+        return goalService.save(goal, roadmap.getId());
+    }
+
+    public Goal generateAndSave(Long roadmapId) {
 
         Roadmap roadmap = roadmapService.findById(roadmapId);
         if (roadmap == null) throw new BusinessException(HttpStatus.NOT_FOUND, ErrorMessageUtils.ERROR_NOT_FOUND.getMessage("Roadmap"));
 
-        String prompt =
-                """
-                Gere um objetivo para o seguinte roadmap: \n{roadmap}
-                \n
-                Leve em conta que a posição do objetivo dentro do roadmap é a posição {roadmapIndex}.
-                \n
-                Quando já houver um objetivo na mesma posição,
-                adicione temas intermediários entre o objetivo de mesma posição e o próximo objetivo ou
-                aprofunde o tema do objetivo anterior. Sempre leve em conta o roadmap como um todo.
-               """;
+        ResponseGenerateGoalDTO generatedGoal = generateGoal(roadmap);
 
-        return ChatClient.create(chatModel).prompt()
-                .user(u -> u.text(prompt)
-                        .param("roadmap", roadmap.toString())
-                        .param("roadmapIndex", roadmapIndex))
-                .call()
-                .entity(ResponseGenerateGoalDTO.class);
+        return saveGenerated(
+                roadmap,
+                goalMapper.toEntity(generatedGoal)
+        );
     }
 
 
