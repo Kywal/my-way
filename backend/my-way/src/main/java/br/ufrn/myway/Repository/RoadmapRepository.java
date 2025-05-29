54f7e05e7b74b226b03c438c1aba78d450f1448b
alisson.diogo.soares <alisson.diogo.soares@gmail.com>
2025-05-28 21:26:45 -0300

    fix(backend): param id to userId

--- backend/my-way/src/main/java/br/ufrn/myway/Repository/RoadmapRepository.java
+++ backend/my-way/src/main/java/br/ufrn/myway/Repository/RoadmapRepository.java
@@ -1,17 +1,18 @@
 package br.ufrn.myway.Repository;
 
-import br.ufrn.myway.Model.Entities.Roadmap;
-import br.ufrn.myway.Model.Enums.RoadMapStatus;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+import br.ufrn.myway.Model.Entities.Roadmap;
+import br.ufrn.myway.Model.Enums.RoadMapStatus;
 
 public interface RoadmapRepository extends SoftDeletableRepository<Roadmap> {
     @Query("SELECT r FROM Roadmap r WHERE r.user.id = :id")
     List<Roadmap> findRoadMapByUser(Long id);
 
     @Query("SELECT r FROM Roadmap r WHERE r.user.id = :userId AND r.status = :status")
-    Roadmap findByStatus(@Param("id") Long userId, @Param("status") RoadMapStatus status);
+    Roadmap findByStatus(@Param("userId") Long userId, @Param("status") RoadMapStatus status);
 
 }
