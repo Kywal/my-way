a99a7533c2c4fda4bff34ab1f6cf872256cd8708
Emanuel Kywal <emanuelkywal@outlook.com>
2025-06-02 00:01:12 -0300

    feat(frontend): app name set to 'My Way'

--- frontend/my-way/config/site.ts
+++ frontend/my-way/config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
+  name: "My Way",
   description: "Make beautiful websites regardless of your design experience.",
   navItems: [
     {
