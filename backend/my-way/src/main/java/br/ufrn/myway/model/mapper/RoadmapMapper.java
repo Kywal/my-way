4ccd8329d0d2a24664c865932399307390a82d48
Emanuel Kywal <emanuelkywal@outlook.com>
2025-07-18 02:52:07 -0300

    feat: RoadmapConcursoPoliciaCivilController and its required mappers

--- backend/my-way/src/main/java/br/ufrn/myway/model/mapper/RoadmapMapper.java
+++ backend/my-way/src/main/java/br/ufrn/myway/model/mapper/RoadmapMapper.java
@@ -4,6 +4,7 @@ import br.ufrn.myway.model.DTO.Request.RequestFullRoadmapDTO;
 import br.ufrn.myway.model.DTO.Request.RequestRoadmapDTO;
 import br.ufrn.myway.model.DTO.Response.ResponseRoadmapDTO;
 import br.ufrn.myway.model.entities.roadmap.RoadmapConcursoGeneralista;
+import br.ufrn.myway.model.entities.roadmap.RoadmapConcursoPoliciaCivil;
 import org.mapstruct.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,9 +17,11 @@ public abstract class RoadmapMapper {
     @Autowired
     protected ConcursoMapper concursoMapper;
 
-
+    public abstract RoadmapConcursoPoliciaCivil toRoadmapConcursoPoliciaCivilEntity(RequestRoadmapDTO request);
+    public abstract RoadmapConcursoPoliciaCivil toRoadmapConcursoPoliciaCivilEntity(RequestFullRoadmapDTO request);
     public abstract RoadmapConcursoGeneralista toEntity(RequestRoadmapDTO roadmapDTO);
     public abstract RoadmapConcursoGeneralista toEntity(RequestFullRoadmapDTO roadmapDTO);
 
+    public abstract ResponseRoadmapDTO toResponse(RoadmapConcursoPoliciaCivil roadmap);
     public abstract ResponseRoadmapDTO toResponse(RoadmapConcursoGeneralista roadmap);
 }
